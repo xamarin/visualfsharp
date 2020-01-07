488de37f27681684cfa7abc3a84dd7cd5edaabd9
nosami <jasonimison@gmail.com>
2020-01-07 04:40:14 +0000

    Clickable hyperlinks

--- vsintegration/src/FSharp.Editor/QuickInfo/WpfNagivableTextRunViewElementFactory.fs
+++ vsintegration/src/FSharp.Editor/QuickInfo/WpfNagivableTextRunViewElementFactory.fs
@@ -9,7 +9,8 @@ open System.Windows
 
 open Microsoft.VisualStudio.Text.Adornments
 open Microsoft.VisualStudio.Text.Editor
-//open Microsoft.VisualStudio.Utilities
+open Microsoft.VisualStudio.Utilities
+type UIElement = Xwt.Widget
 
 [<Export(typeof<IViewElementFactory>)>]
 [<Name("NavigableTextRun to UIElement")>]
@@ -21,7 +22,7 @@ type WpfNavigableTextRunViewElementFactory
         viewElementFactoryService:IViewElementFactoryService,
         settings: EditorOptions
     ) =
-    let styles = Microsoft.VisualStudio.FSharp.UIResources.NavStyles()
+    //let styles = Microsoft.VisualStudio.FSharp.UIResources.NavStyles()
     interface IViewElementFactory with
         override __.CreateViewElement<'TView when 'TView: not struct>(textView:ITextView, model:obj) : 'TView =
             if not (model :? NavigableTextRun) || typeof<'TView> <> typeof<UIElement> then
@@ -32,32 +33,34 @@ type WpfNavigableTextRunViewElementFactory
             let classifiedTextRun = ClassifiedTextRun(navigableTextRun.ClassificationTypeName, navigableTextRun.Text)
             let classifiedTextElement = ClassifiedTextElement([classifiedTextRun])
             let convertedElement = viewElementFactoryService.CreateViewElement<UIElement>(textView, classifiedTextElement)
+            let link = new Xwt.LinkLabel(navigableTextRun.Text)
+            //link.Text <- navigableTextRun.Text
+            //link :> 'TView
+            //// apply HTML-like styles
+            //match convertedElement with
+            //| :? TextBlock as tb ->
+                //let underlineStyle =
+                //    let key =
+                //        if settings.QuickInfo.DisplayLinks then
+                //            match settings.QuickInfo.UnderlineStyle with
+                //            | QuickInfoUnderlineStyle.Solid -> "solid_underline"
+                //            | QuickInfoUnderlineStyle.Dash -> "dash_underline"
+                //            | QuickInfoUnderlineStyle.Dot -> "dot_underline"
+                //        else
+                //            "no_underline"
+                //    styles.Resources.[key] :?> Style
 
-            // apply HTML-like styles
-            match convertedElement with
-            | :? TextBlock as tb ->
-                let underlineStyle =
-                    let key =
-                        if settings.QuickInfo.DisplayLinks then
-                            match settings.QuickInfo.UnderlineStyle with
-                            | QuickInfoUnderlineStyle.Solid -> "solid_underline"
-                            | QuickInfoUnderlineStyle.Dash -> "dash_underline"
-                            | QuickInfoUnderlineStyle.Dot -> "dot_underline"
-                        else
-                            "no_underline"
-                    styles.Resources.[key] :?> Style
-
-                // we need to enclose the generated Inline, which is actually a Run element, 
-                // because fancy styling does not seem to work properly with Runs
-                let inl = tb.Inlines.FirstInline
-                let color = inl.Foreground
-                // clear the color here to make it inherit
-                inl.ClearValue(Documents.TextElement.ForegroundProperty) |> ignore
-                // this constructor inserts into TextBlock
-                Documents.Underline(tb.Inlines.FirstInline, tb.ContentStart, Foreground = color) |> ignore
-                tb.Resources.[typeof<Documents.Underline>] <- underlineStyle
-            | _ -> ()
+                //// we need to enclose the generated Inline, which is actually a Run element, 
+                //// because fancy styling does not seem to work properly with Runs
+                //let inl = tb.Inlines.FirstInline
+                //let color = inl.Foreground
+                //// clear the color here to make it inherit
+                //inl.ClearValue(Documents.TextElement.ForegroundProperty) |> ignore
+                //// this constructor inserts into TextBlock
+                //Documents.Underline(tb.Inlines.FirstInline, tb.ContentStart, Foreground = color) |> ignore
+                //tb.Resources.[typeof<Documents.Underline>] <- underlineStyle
+            //| _ -> ()
 
             // add navigation
-            //convertedElement.MouseDown.Add(fun _ -> navigableTextRun.NavigateAction())
-            convertedElement :> obj :?> 'TView
+            link.ButtonReleased.Add(fun _ -> navigableTextRun.NavigateAction())
+            link :> obj :?> 'TView
