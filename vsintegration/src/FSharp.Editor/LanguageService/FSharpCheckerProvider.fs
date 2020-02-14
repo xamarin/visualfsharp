e7fdf9e3214b595d4ea109f3155f17325637955b
Will Smith <lol.tihan@gmail.com>
2020-02-13 16:43:09 -0800

    Optimized find all references and reduced memory usage in VS (#8339)
    * Added ItemKey.fsi/fsi. Added blank SemanticClassification.fs/fsi.
    
    * Raise disposed exception
    
    * Re-worked semantic classification. Renamed ItemKeyReader to ItemKeyStore. Exposing ItemKeyStore/Builder
    
    * Fixing build
    
    * Storing semantic classification
    
    * Caching semantic classification
    
    * Wiring it up
    
    * Need to fix lexing
    
    * Added experimental lexing API to handle find all refs syntactic classification from allocating a lot
    
    * Added System.Memory
    
    * Using Span to check equality without allocating
    
    * Allocate less
    
    * Fixing build. Reducing more allocations and not using lex filter on lexing tokens.
    
    * Remove langversion
    
    * Fixed record find all refs
    
    * Fixing test
    
    * Partial match for active pattern
    
    * Feedback changes
    
    * Added comment on TcResolutionsExtensions
    
    * Creating view accessor when needed in ItemKey. Fixed UnionCase find all refs.
    
    * Added comment on warning
    
    * Added Range.comparer. Moving opens to top of file
    
    * More feedback changes
    
    * Added comment on sliding expiration

--- vsintegration/src/FSharp.Editor/LanguageService/FSharpCheckerProvider.fs
+++ vsintegration/src/FSharp.Editor/LanguageService/FSharpCheckerProvider.fs
@@ -12,8 +12,7 @@ open Microsoft.VisualStudio.FSharp.Editor
 open Microsoft.VisualStudio.LanguageServices
 open FSharp.NativeInterop
 open Microsoft.CodeAnalysis.ExternalAccess.FSharp.Diagnostics
-open MonoDevelop.Ide
-open MonoDevelop.Ide.TypeSystem
+
 #nowarn "9" // NativePtr.toNativeInt
 
 // Exposes FSharpChecker as MEF export
@@ -22,31 +21,31 @@ type internal FSharpCheckerProvider
     [<ImportingConstructor>]
     (
         analyzerService: IFSharpDiagnosticAnalyzerService,
+        [<Import(typeof<VisualStudioWorkspace>)>] workspace: VisualStudioWorkspace,
         settings: EditorOptions
     ) =
-    let workspace = IdeApp.TypeSystemService.Workspace :?> MonoDevelopWorkspace
-    let tryGetMetadataSnapshot (_path, _timeStamp) = 
 
-        //try
-        //    let md = Microsoft.CodeAnalysis.ExternalAccess.FSharp.LanguageServices.FSharpVisualStudioWorkspaceExtensions.GetMetadata(workspace, path, timeStamp)
-        //    let amd = (md :?> AssemblyMetadata)
-        //    let mmd = amd.GetModules().[0]
-        //    let mmr = mmd.GetMetadataReader()
+    let tryGetMetadataSnapshot (path, timeStamp) = 
+        try
+            let md = Microsoft.CodeAnalysis.ExternalAccess.FSharp.LanguageServices.FSharpVisualStudioWorkspaceExtensions.GetMetadata(workspace, path, timeStamp)
+            let amd = (md :?> AssemblyMetadata)
+            let mmd = amd.GetModules().[0]
+            let mmr = mmd.GetMetadataReader()
 
-        //    // "lifetime is timed to Metadata you got from the GetMetadata(...). As long as you hold it strongly, raw 
-        //    // memory we got from metadata reader will be alive. Once you are done, just let everything go and 
-        //    // let finalizer handle resource rather than calling Dispose from Metadata directly. It is shared metadata. 
-        //    // You shouldn't dispose it directly."
+            // "lifetime is timed to Metadata you got from the GetMetadata(...). As long as you hold it strongly, raw 
+            // memory we got from metadata reader will be alive. Once you are done, just let everything go and 
+            // let finalizer handle resource rather than calling Dispose from Metadata directly. It is shared metadata. 
+            // You shouldn't dispose it directly."
 
-        //    let objToHold = box md
+            let objToHold = box md
 
-        //    // We don't expect any ilread WeakByteFile to be created when working in Visual Studio
-        //    Debug.Assert((FSharp.Compiler.AbstractIL.ILBinaryReader.GetStatistics().weakByteFileCount = 0), "Expected weakByteFileCount to be zero when using F# in Visual Studio. Was there a problem reading a .NET binary?")
+            // We don't expect any ilread WeakByteFile to be created when working in Visual Studio
+            Debug.Assert((FSharp.Compiler.AbstractIL.ILBinaryReader.GetStatistics().weakByteFileCount = 0), "Expected weakByteFileCount to be zero when using F# in Visual Studio. Was there a problem reading a .NET binary?")
 
-        //    Some (objToHold, NativePtr.toNativeInt mmr.MetadataPointer, mmr.MetadataLength)
-        //with ex -> 
-            //// We catch all and let the backup routines in the F# compiler find the error
-            //Assert.Exception(ex)
+            Some (objToHold, NativePtr.toNativeInt mmr.MetadataPointer, mmr.MetadataLength)
+        with ex -> 
+            // We catch all and let the backup routines in the F# compiler find the error
+            Assert.Exception(ex)
             None 
 
 
@@ -58,8 +57,10 @@ type internal FSharpCheckerProvider
                     keepAllBackgroundResolutions = false,
                     // Enabling this would mean that if devenv.exe goes above 2.3GB we do a one-off downsize of the F# Compiler Service caches
                     (* , MaxMemory = 2300 *)
-                    (*legacyReferenceResolver=LegacyMSBuildReferenceResolver.getResolver(),*)
-                    tryGetMetadataSnapshot = tryGetMetadataSnapshot)
+                    legacyReferenceResolver=LegacyMSBuildReferenceResolver.getResolver(),
+                    tryGetMetadataSnapshot = tryGetMetadataSnapshot,
+                    keepAllBackgroundSymbolUses = false,
+                    enableBackgroundItemKeyStoreAndSemanticClassification = true)
 
             // This is one half of the bridge between the F# background builder and the Roslyn analysis engine.
             // When the F# background builder refreshes the background semantic build context for a file,
