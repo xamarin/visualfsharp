98c80db6e93a40428df181ca5ca7270ab1bd8185
nosami <jasonimison@gmail.com>
2020-04-30 14:45:38 +0100

    Move PortableFSharpProjectFlavor to FSharp.Editor project

---
+++ vsintegration/src/FSharp.Editor/VSMac/PortableFSharpProjectFlavor.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharp.Editor
+
+open MonoDevelop.Projects
+
+type PortableFSharpProjectFlavor() =
+    inherit PortableDotNetProjectFlavor()
+
+    override x.OnGetDefaultImports imports =
+        imports.Add @"$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\FSharp\Microsoft.Portable.FSharp.Targets"
