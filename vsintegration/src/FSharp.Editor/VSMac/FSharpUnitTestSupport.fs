d818e67a15b57ed4168354d65567119dba530752
Kirill Osenkov <github@osenkov.com>
2020-07-22 15:04:27 -0700

    Unit-test progress

--- vsintegration/src/FSharp.Editor/VSMac/FSharpUnitTestSupport.fs
+++ vsintegration/src/FSharp.Editor/VSMac/FSharpUnitTestSupport.fs
@@ -30,6 +30,7 @@ open System
 open System.Composition
 open System.Collections.Generic
 open System.Diagnostics
+open System.Linq
 open System.Threading
 
 open FSharp.Compiler.SourceCodeServices
@@ -39,6 +40,7 @@ open Microsoft.CodeAnalysis.Classification
 open Microsoft.CodeAnalysis.Editor
 open Microsoft.CodeAnalysis.Host.Mef
 open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.Text.Shared.Extensions
 open Microsoft.VisualStudio.Text
 open Microsoft.VisualStudio.Text.Editor
 open Microsoft.CodeAnalysis.ExternalAccess.FSharp.Classification
@@ -47,6 +49,7 @@ open Microsoft.VisualStudio.FSharp.Editor
 open Microsoft.VisualStudio.Text.Classification
 open Microsoft.VisualStudio.Text.Tagging
 
+open Mono.Addins
 open MonoDevelop.Core
 open Microsoft.CodeAnalysis.ExternalAccess.FSharp.Editor
 
@@ -65,68 +68,69 @@ module Logic =
             |> Seq.exists (filter name)
         | None -> false
 
-    let gatherUnitTests (unitTestMarkers: IUnitTestMarkers[], allSymbols:FSharpSymbolUse [] option) =
+    let gatherUnitTests (snapshot: ITextSnapshot, unitTestMarkers: IUnitTestMarkers[], allSymbols:FSharpSymbolUse []) =
         let hasAttribute a = hasAttributeNamed a unitTestMarkers
         let tests = ResizeArray<UnitTestLocation>()
-        
-        let testSymbols = 
-            match allSymbols with
-            | None -> None
-            | Some symbols ->
-                symbols 
-                |> Array.filter
-                    (fun s -> match s.Symbol with
-                              | :? FSharpMemberOrFunctionOrValue as fom -> 
-                                  fom.Attributes
-                                  |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.TestMethodAttributeMarker = attributeName || m.TestCaseMethodAttributeMarker = attributeName) )
-                              | :? FSharpEntity as fse -> 
-                                      fse.MembersFunctionsAndValues
-                                      |> Seq.exists (fun fom -> fom.Attributes
-                                                                |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.TestMethodAttributeMarker = attributeName || m.TestCaseMethodAttributeMarker = attributeName) ))
-                              | _ -> false )
-                |> Seq.distinctBy (fun su -> su.RangeAlternate)
-                |> Seq.choose
-                    (fun symbolUse -> 
-                        let range = symbolUse.RangeAlternate
-                        let startOffset = editor.LocationToOffset(range.StartLine, range.StartColumn+1)
-                        let test = UnitTestLocation(startOffset)
-                        match symbolUse.Symbol with
-                        | :? FSharpMemberOrFunctionOrValue as func -> 
-                            let typeName =
-                                match func.DeclaringEntity with
-                                | Some ent -> ent.QualifiedName
-                                | None _ ->
-                                    MonoDevelop.Core.LoggingService.LogWarning(sprintf "F# GatherUnitTests: found a unit test method with no qualified name: %s" func.FullName)
-                                    func.CompiledName
-                            let methName = func.CompiledName
-                            let isIgnored =
-                                func.Attributes
-                                |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.IgnoreTestMethodAttributeMarker = attributeName))
-                            //add test cases
-                            let testCases =
-                                func.Attributes
-                                |> Seq.filter (fun a -> hasAttribute a (fun attributeName m -> m.TestCaseMethodAttributeMarker = attributeName))
-                            testCases
-                            |> Seq.map createTestCase
-                            |> test.TestCases.AddRange
-                            test.UnitTestIdentifier <- typeName + "." + methName
-                            test.IsIgnored <- isIgnored
-                            Some test
-                        | :? FSharpEntity as entity ->
-                            let typeName = entity.QualifiedName
-                            let isIgnored =
-                                entity.Attributes
-                                |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.IgnoreTestMethodAttributeMarker = attributeName))
-                            test.UnitTestIdentifier <- typeName
-                            test.IsIgnored <- isIgnored
-                            test.IsFixture <- true
-                            Some test
-                        | _ -> None)
-                |> Some
+
+        let testSymbols =
+            allSymbols
+            |> Array.filter
+                (fun s -> match s.Symbol with
+                          | :? FSharpMemberOrFunctionOrValue as fom -> 
+                              fom.Attributes
+                              |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.TestMethodAttributeMarker = attributeName || m.TestCaseMethodAttributeMarker = attributeName) )
+                          | :? FSharpEntity as fse -> 
+                                  fse.MembersFunctionsAndValues
+                                  |> Seq.exists (fun fom -> fom.Attributes
+                                                            |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.TestMethodAttributeMarker = attributeName || m.TestCaseMethodAttributeMarker = attributeName) ))
+                          | _ -> false )
+            |> Seq.distinctBy (fun su -> su.RangeAlternate)
+            |> Seq.choose
+                (fun symbolUse -> 
+                    let range = symbolUse.RangeAlternate
+
+                    let startOffset = RoslynHelpers. snapshot.GetPosition (range.StartLine, range.StartColumn+1)
+                    let test = UnitTestLocation(startOffset)
+                    match symbolUse.Symbol with
+                    | :? FSharpMemberOrFunctionOrValue as func -> 
+                        let typeName =
+                            match func.DeclaringEntity with
+                            | Some ent -> ent.QualifiedName
+                            | None _ ->
+                                MonoDevelop.Core.LoggingService.LogWarning(sprintf "F# GatherUnitTests: found a unit test method with no qualified name: %s" func.FullName)
+                                func.CompiledName
+                        let methName = func.CompiledName
+                        let isIgnored =
+                            func.Attributes
+                            |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.IgnoreTestMethodAttributeMarker = attributeName))
+                        //add test cases
+                        let testCases =
+                            func.Attributes
+                            |> Seq.filter (fun a -> hasAttribute a (fun attributeName m -> m.TestCaseMethodAttributeMarker = attributeName))
+                        testCases
+                        |> Seq.map createTestCase
+                        |> test.TestCases.AddRange
+                        test.UnitTestIdentifier <- typeName + "." + methName
+                        test.IsIgnored <- isIgnored
+                        Some test
+                    | :? FSharpEntity as entity ->
+                        let typeName = entity.QualifiedName
+                        let isIgnored =
+                            entity.Attributes
+                            |> Seq.exists (fun a -> hasAttribute a (fun attributeName m -> m.IgnoreTestMethodAttributeMarker = attributeName))
+                        test.UnitTestIdentifier <- typeName
+                        test.IsIgnored <- isIgnored
+                        test.IsFixture <- true
+                        Some test
+                    | _ -> None)
+            |> Some
         testSymbols
         |> Option.iter tests.AddRange 
         tests
 
+type FSharpUnitTestTag (ids: seq<string>) =
+    interface IUnitTestTag with
+        member __.TestIds = ids
 
 type FSharpUnitTestTagger(textView, checkerProvider: FSharpCheckerProvider, projectInfoManager: FSharpProjectOptionsManager) =
     let tagsChanged = Event<_,_>()
@@ -140,13 +144,23 @@ type FSharpUnitTestTagger(textView, checkerProvider: FSharpCheckerProvider, proj
                 asyncMaybe {
                     let snapshot = collection.[0].Snapshot
                     let document = snapshot.GetOpenDocumentInCurrentContextWithChanges()
-                    let sourceText = document.GetTextAsync(CancellationToken.None)
+                    let! sourceText = document.GetTextAsync(CancellationToken.None)
                     let! _, _, projectOptions = projectInfoManager.TryGetOptionsForDocumentOrProject(document, CancellationToken.None)
 
                     let! _, _, checkResults = checkerProvider.Checker.ParseAndCheckDocument(document, projectOptions, sourceText = sourceText, allowStaleResults = false, userOpName=userOpName)
                     let! symbols = checkResults.GetAllUsesOfAllSymbolsInFile() |> liftAsync
 
-                    
+                    let unitTestMarkers = AddinManager.GetExtensionNodes("/MonoDevelop/UnitTesting/UnitTestMarkers").OfType<IUnitTestMarkers>().ToArray()
+
+                    let tests = Logic.gatherUnitTests (snapshot, unitTestMarkers, symbols)
+
+                    for test in tests do
+                        test
+
+                        let textSpan = RoslynHelpers.FSharpRangeToTextSpan (sourceText, range)
+                        let snapshotSpan = SnapshotSpan(snapshot, textSpan.Start, textSpan.Length)
+                        let tag = FSharpUnitTestTag(id)
+                        let tagSpan = TagSpan<IUnitTestTag> (snapshotSpan, tag) 
 
                     return 1
                 }
@@ -155,9 +169,7 @@ type FSharpUnitTestTagger(textView, checkerProvider: FSharpCheckerProvider, proj
 
         
 
-type FSharpUnitTestTag (ids: seq<string>) =
-    interface IUnitTestTag with
-        member __.TestIds = ids
+
 
 [<Export(typeof<IViewTaggerProvider>)>]
 [<Microsoft.VisualStudio.Utilities.ContentType(FSharpConstants.FSharpContentTypeName)>]
