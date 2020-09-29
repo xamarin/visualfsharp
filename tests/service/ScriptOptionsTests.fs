f6f9826789ca722e6469d76be1b6e8951177e068
Don Syme <donsyme@fastmail.com>
2020-09-29 15:43:40 +0100

    fix script compilation

--- tests/service/ScriptOptionsTests.fs
+++ tests/service/ScriptOptionsTests.fs
@@ -22,36 +22,13 @@ let pi = Math.PI
 [<TestCase(true, false, [| "--targetprofile:mscorlib" |])>]
 [<TestCase(false, true, [| "--targetprofile:netcore" |])>]
 [<Test>]
-let ``can generate options for different frameworks regardless of execution environment``(assumeNetFx, useSdk, flags) =
+let ``can generate options for different frameworks regardless of execution environment``(defaultToDotNetFramework, useSdk, flags) =
     let path = Path.GetTempPath()
     let file = Path.GetTempFileName()
     let tempFile = Path.Combine(path, file)
     let (_, errors) =
-        checker.GetProjectOptionsFromScript(tempFile, SourceText.ofString scriptSource, assumeDotNetFramework = assumeNetFx, useSdkRefs = useSdk, otherFlags = flags)
+        checker.GetProjectOptionsFromScript(tempFile, SourceText.ofString scriptSource, defaultToDotNetFramework = defaultToDotNetFramework, useSdkRefs = useSdk, otherFlags = flags)
         |> Async.RunSynchronously
     match errors with
     | [] -> ()
-    | errors -> failwithf "Error while parsing script with assumeDotNetFramework:%b, useSdkRefs:%b, and otherFlags:%A:\n%A" assumeNetFx useSdk flags errors
-
-[<TestCase(true, false, [| "--targetprofile:mscorlib" |])>]
-[<TestCase(false, true, [| "--targetprofile:netcore" |])>]
-[<Test>]
-let ``all default assembly references are system assemblies``(assumeNetFx, useSdk, flags) =
-    let path = Path.GetTempPath()
-    let file = Path.GetTempFileName()
-    let tempFile = Path.Combine(path, file)
-    let (options, errors) =
-        checker.GetProjectOptionsFromScript(tempFile, SourceText.ofString scriptSource, assumeDotNetFramework = assumeNetFx, useSdkRefs = useSdk, otherFlags = flags)
-        |> Async.RunSynchronously
-    match errors with
-    | [] -> ()
-    | errors -> failwithf "Error while parsing script with assumeDotNetFramework:%b, useSdkRefs:%b, and otherFlags:%A:\n%A" assumeNetFx useSdk flags errors
-    for r in options.OtherOptions do 
-        if r.StartsWith("-r:") then 
-            let ref = Path.GetFullPath(r.[3..])
-            let baseName = Path.GetFileNameWithoutExtension(ref)
-            if not (FSharp.Compiler.DotNetFrameworkDependencies.systemAssemblies.Contains(baseName)) then
-                printfn "Failing, printing options from GetProjectOptionsFromScript..."
-                for opt in options.OtherOptions do
-                    printfn "option: %s" opt
-                failwithf "expected FSharp.Compiler.DotNetFrameworkDependencies.systemAssemblies to contain '%s' because '%s' is a default reference for a script, (assumeNetFx, useSdk, flags) = %A" baseName ref (assumeNetFx, useSdk, flags) 
+    | errors -> failwithf "Error while parsing script with assumeDotNetFramework:%b, useSdkRefs:%b, and otherFlags:%A:\n%A" defaultToDotNetFramework useSdk flags errors
